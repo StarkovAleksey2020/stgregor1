0a1d29d241121ee02c8443a99263fd0ee7227f63
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2020-12-26 09:04:46 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+  if ($(window).scrollTop() > 200) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+  var certificatesSlider = new Swiper('.certificates-slider', {
+    // Optional parameters
+    loop: true,
+  
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      prevEl: '.certificates-buttons__left',
+      nextEl: '.certificates-buttons__right',
+    },
+  });
+  var certificatesSlider = new Swiper('.portfolio-slider', {
+    // Optional parameters
+    loop: true,
+  
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      prevEl: '.portfolio-buttons__left',
+      nextEl: '.portfolio-buttons__right',
+    },
+  });
+
+
+    $(window).scroll(function(){
+      $('.skills').bgscroll({
+        direction: 'bottom', // направление bottom или top
+        bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+        debug: false, // Режим отладки
+        min:0, // минимальное положение (в %) на которое может смещаться фон
+        max:100 // максимальное положение (в %) на которое может смещаться фон
+      });
+    })
+    $(window).scroll(function(){
+      $('.works').bgscroll({
+        direction: 'bottom', // направление bottom или top
+        bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+        debug: false, // Режим отладки
+        min:0, // минимальное положение (в %) на которое может смещаться фон
+        max:100 // максимальное положение (в %) на которое может смещаться фон
+      });
+    })
+
+});
